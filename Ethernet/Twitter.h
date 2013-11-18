5c6c7c5567145a231e2b29085e41ecf417b354ce
KISSMonX <omonkerman@foxmail.com>
2013-11-18 16:06:26 +0800

    用自己的代码风格进行了格式化，看着舒服多了。

--- Ethernet/Twitter.h
+++ Ethernet/Twitter.h
@@ -1,10 +1,10 @@
 /*
   Twitter.cpp - Arduino library to Post messages to Twitter using OAuth.
   Copyright (c) NeoCat 2010-2011. All right reserved.
-  
+
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
 
 // ver1.2 - Use <Udp.h> to support IDE 0019 or later
@@ -26,21 +26,23 @@
 class Twitter
 {
 private:
-	uint8_t parseStatus;
-	int statusCode;
-	const char *token;
+        uint8_t parseStatus;
+        int statusCode;
+        const char *token;
 #if defined(ARDUINO) && ARDUINO < 100
-	Client client;
+        Client client;
 #else
-	EthernetClient client;
+        EthernetClient client;
 #endif
 public:
-	Twitter(const char *user_and_passwd);
-	
-	bool post(const char *msg);
-	bool checkStatus(Print *debug = NULL);
-	int  wait(Print *debug = NULL);
-	int  status(void) { return statusCode; }
+        Twitter(const char *user_and_passwd);
+
+        bool post(const char *msg);
+        bool checkStatus(Print *debug = NULL);
+        int  wait(Print *debug = NULL);
+        int  status(void) {
+                return statusCode;
+        }
 };
 
 #endif	//TWITTER_H
